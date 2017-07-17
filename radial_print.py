d8710ac0ad62f2c1cd88281b5aa8ca19e4422a62
Aaron Shaver <aaron.shaver@jivesoftware.com>
2017-07-17 16:59:11 -0700

    Getting close to done on radial print

--- radial_print.py
+++ radial_print.py
@@ -1,26 +1,43 @@
 import sys
 
 
-class RadialPrint:
+def stringify(grid):
+    output = ''
+    for line in grid:
+        for char in line:
+            output += char
+        output += '\n'
+    return output[0:-1]  # remove extra line break
 
-    @staticmethod
-    def radial(text):
-        size = (len(text) * 2) - 1
-        if size < 0:
-            return ''
-        grid = [['.' for x in range(size)] for y in range(size)]
 
-        output = ''
-        for line in grid:
-            for char in line:
-                output += char
-            output += '\n'
-        output = output[0:-1]
-        return output
+def radial_print(text):
+    text_length = len(text)
+    origin_coord = text_length - 1
+    grid_side_length = (text_length * 2) - 1
+    if grid_side_length < 0:
+        return ''
+
+    # initialize grid
+    grid = [[' ' for x in range(grid_side_length)] \
+        for y in range(grid_side_length)]
+
+    x, y = origin_coord, origin_coord
+
+    j = 0
+    for i in range(text_length):
+        if i == 0:
+            grid[y][x] = text[0]
+        else:
+            grid[y][x-i] = text[j]  # left
+            grid[y][x+i] = text[j]  # right
+            grid[y+i][x] = text[j]  # down
+            grid[y-i][x] = text[j]  # up
+        j += 1
+    return stringify(grid)
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        print(RadialPrint.radial(sys.argv[1]))
+        print(radial_print(sys.argv[1]))
     else:
         print('Please supply text to radial print')
--- test_radial_print.py
+++ test_radial_print.py
@@ -1,20 +1,17 @@
-from radial_print import RadialPrint
+from radial_print import radial_print 
 import unittest
 
 class TestRadialPrint(unittest.TestCase):
 
-    def setUp(self):
-        self.r = RadialPrint()
-
     def test_single_letter(self):
-        self.assertEqual('a', self.r.radial('a'))
+        self.assertEqual('a', radial_print('a'))
 
     def test_empty(self):
-        self.assertEqual('', self.r.radial(''))
+        self.assertEqual('', radial_print(''))
 
     def test_two_letter(self):
-        self.assertEqual('sss\nsis\nsss', self.r.radial('is'))
+        self.assertEqual('sss\nsis\nsss', radial_print('is'))
 
     def test_three_letter(self):
         self.assertEqual('e e e\n hhh \nehthe\n hhh \ne e e\n',
-            self.r.radial('the'))
+            radial_print('the'))
