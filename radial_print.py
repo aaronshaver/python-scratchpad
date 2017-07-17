735bd38f8b340288ec57b406599b38b394d4cb5b
Aaron Shaver <aaron.shaver@jivesoftware.com>
2017-07-17 16:11:05 -0700

    more logic

--- radial_print.py
+++ radial_print.py
@@ -6,9 +6,21 @@ class RadialPrint:
     @staticmethod
     def radial(text):
         size = (len(text) * 2) - 1
-        return text
+        if size < 0:
+            return ''
+        grid = [['.' for x in range(size)] for y in range(size)]
+
+        output = ''
+        for line in grid:
+            for char in line:
+                output += char
+            output += '\n'
+        output = output[0:-1]
+        return output
 
 
 if __name__ == "__main__":
-
-    print(RadialPrint.radial(sys.argv[0]))
+    if len(sys.argv) > 1:
+        print(RadialPrint.radial(sys.argv[1]))
+    else:
+        print('Please supply text to radial print')
