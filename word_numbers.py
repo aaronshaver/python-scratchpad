3c83d3c459fd745904905b229fcf45706f7efcb6
Aaron Shaver <AaronShaverPDX@gmail.com>
2019-10-12 10:03:57 -0700

    Can now do any single digit positive number

--- word_numbers.py
+++ word_numbers.py
@@ -10,13 +10,25 @@ import math
 
 class WordNumbers():
 
-    @staticmethod
-    def get_digit_count(number):
+    def get_digit_count(self, number):
         if number > 0:
             return int(math.log10(number))+1
         elif number == 0:
             return 1
 
-    @staticmethod
-    def convert(number):
-        return "one"
+    def convert(self, number):
+        if self.get_digit_count(number) == 1:
+            return self.single_digit_numbers[number]
+
+    single_digit_numbers = {
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+    }
--- word_numbers_test.py
+++ word_numbers_test.py
@@ -5,17 +5,47 @@ class Tests(unittest.TestCase):
     # run tests with this command:
     # python -m unittest word_numbers_test.py
 
+    def setUp(self):
+        self.converter = WordNumbers()
+        
     def test_get_digit_count_0(self):
-        self.assertEqual(1, WordNumbers.get_digit_count(0))
+        self.assertEqual(1, self.converter.get_digit_count(0))
 
     def test_get_digit_count_1(self):
-        self.assertEqual(1, WordNumbers.get_digit_count(1))
+        self.assertEqual(1, self.converter.get_digit_count(1))
 
     def test_get_digit_count_10(self):
-        self.assertEqual(2, WordNumbers.get_digit_count(10))
+        self.assertEqual(2, self.converter.get_digit_count(10))
 
     def test_get_digit_count_999(self):
-        self.assertEqual(3, WordNumbers.get_digit_count(999))
+        self.assertEqual(3, self.converter.get_digit_count(999))
 
-    def test_number_one(self):
-        self.assertEqual("one", WordNumbers.convert(1))
+    def test_convert_0(self):
+        self.assertEqual("zero", self.converter.convert(0))
+
+    def test_convert_1(self):
+        self.assertEqual("one", self.converter.convert(1))
+
+    def test_convert_2(self):
+        self.assertEqual("two", self.converter.convert(2))
+
+    def test_convert_3(self):
+        self.assertEqual("three", self.converter.convert(3))
+
+    def test_convert_4(self):
+        self.assertEqual("four", self.converter.convert(4))
+
+    def test_convert_5(self):
+        self.assertEqual("five", self.converter.convert(5))
+
+    def test_convert_6(self):
+        self.assertEqual("six", self.converter.convert(6))
+
+    def test_convert_7(self):
+        self.assertEqual("seven", self.converter.convert(7))
+
+    def test_convert_8(self):
+        self.assertEqual("eight", self.converter.convert(8))
+
+    def test_convert_9(self):
+        self.assertEqual("nine", self.converter.convert(9))
