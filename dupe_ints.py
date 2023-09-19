210a902827ea92ba5075f478622efb978de48ccc
Aaron Shaver <aaronshaverpdx@gmail.com>
2023-09-19 16:05:42 -0700

    wrote a O(n) algorithm and did various testing on it; added new data set that is comprised of mixed data

--- dupe_ints.py
+++ dupe_ints.py
@@ -2,7 +2,9 @@
 # return the value of the first duplicate in a list of integers
 # return null if there are no dupes
 
-import time
+import time, random
+
+ITERATIONS = 1000
 
 nums1 = [1, 4, 8, 3, 7, 1, 2, 6, 19, -4]
 nums2 = [0, 4, 8, 3, 7, 1, 2, 6, 19, -4]
@@ -10,6 +12,14 @@ nums3 = [-4, 0, 4, 8, 3, 7, 1, 2, 6, 19, -4]
 nums4 = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 nums_no_dupes = [x for x in range(1000)]
 nums_dupe = [-1] + [x for x in range(10000)] + [-1]
+middle_dupes = [x for x in range(1001,2001)] + [-1,-1] + [x for x in range(1000)]
+
+# better simulate "real" data by shuffling a dupe into a random list
+# run several times for best results, as dupe location will vary between runs
+random_nums = [random.randint(0, 10000) for _ in range (9999)]
+duplicate = [random.choice(random_nums) for _ in range(1)]
+mixed_data = random_nums + duplicate
+random.shuffle(mixed_data)
 
 def first_dupe(numbers):
     if len(numbers) == len(set(numbers)):
@@ -28,22 +38,26 @@ def first_dupe_v2(numbers):
                 return numbers[i]
     return None
 
-# print('expect 1, get ' + str(first_dupe(nums1)))
-# print('expect None, get ' + str(first_dupe(nums2)))
-# print('expect -4, get ' + str(first_dupe(nums3)))
-# print('expect 3, get ' + str(first_dupe(nums4)))
-
-# print()
-
-# print('expect 1, get ' + str(first_dupe_v2(nums1)))
-# print('expect None, get ' + str(first_dupe_v2(nums2)))
-# print('expect -4, get ' + str(first_dupe_v2(nums3)))
-# print('expect 3, get ' + str(first_dupe_v2(nums4)))
+def first_dupe_v3(numbers):
+    seen = set()
+    for number in numbers:
+        old_length = len(seen)
+        seen.add(number)
+        if len(seen) == old_length:  # dupes don't add to the length
+            return number
+    return None
 
 if __name__ == '__main__':
     from timeit import Timer
-    test_data = nums_dupe  # so we can switch out test sets easily
+
+    # use a var so we can switch out test sets easily
+    test_data = mixed_data
+
     t = Timer("first_dupe(test_data)", "from __main__ import first_dupe, test_data")
-    print(t.timeit(number=1000))
+    print(t.timeit(number=ITERATIONS))
+
     t = Timer("first_dupe_v2(test_data)", "from __main__ import first_dupe_v2, test_data")
-    print(t.timeit(number=1000))
+    print(t.timeit(number=ITERATIONS))
+
+    t = Timer("first_dupe_v3(test_data)", "from __main__ import first_dupe_v3, test_data")
+    print(t.timeit(number=ITERATIONS))
